84929ec6707884fa7df0a50571d5a68cc8096ae5
yudaiishiyama <yude3939@gmail.com>
2022-07-03 09:21:45 +0900

    ユーザー情報編集

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,19 +2,30 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
   end
-
+  
   def edit
     @user = User.find(params[:id])
   end
 
   def update
     @user = User.find(params[:id])
-    @user.update(user_params)
-    redirect_to user_path(@user)
+
+    if current_user == @user
+      if @user.update(user_params)
+        flash[:success] = 'プロフィール画像を変更しました'
+        render :show
+      else
+        flash.now[:danger] = 'プロフィール画像の変更に失敗しました'
+        render :show
+      end
+    else
+      redirect_to user_path
+    end
   end
 
   private
+
   def user_params
-    params.require(:user).permit(:name, :email, :profile, :image, :password)
+    params.require(:user).permit(:image)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,9 +3,25 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  def update_without_current_password(params, *options)
+    params.delete(:current_password)
+      
+          
+    
+  if params[:password].blank? && params[:password_confirmation].blank?
+    params.delete(:password)
+    params.delete(:password_confirmation)
+  end
+
+  result = update_attributes(params, *options)
+  clean_up_passwords
+  result
+  end
+
   validates :name, presence: true
   VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
-  validates :password, format: { with: VALID_PASSWORD_REGEX }, presence: true
+  validates :password, format: { with: VALID_PASSWORD_REGEX }, presence: true, if: :password_required?
   validates :image, presence: true
   validates :profile, presence: true
 
